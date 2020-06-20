b365325f53704800a2c5d45519698744843684c3
ivanz <ivanz@DESKTOP-SD2PMFE>
2020-06-20 12:20:07 -0500

    ARCA-ESTRUCTURADO

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html>     
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -7,11 +7,264 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>ARCA</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <ul id="dropdown1" class="dropdown-content">
+            <li><a href="#!">Perros</a></li>
+            <li><a href="#!">Gatos</a></li>
+            <li class="divider"></li>
+            <li><a href="#!">Otro</a></li>
+        </ul>
+        <nav>
+            <div class="nav-wrapper">
+                <img src="" alt="" width="80px" class="brand-logo center">
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Nuevos amigos<span class="new badge">5</span></a></li>
+                    <li><a href="badges.html">Contacto</a></li>
+                    <!-- Dropdown Trigger -->
+                    <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Tipo de mascota<i
+                                class="material-icons right">arrow_drop_down</i></a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="slider">
+            <ul class="slides">
+                <li>
+                    <img
+                        src="https://static.nationalgeographic.es/files/styles/image_3200/public/01-stray-dogs-nationalgeographic_1927666.jpg">
+                    <!-- random image -->
+                    <div class="caption center-align">
+                        <h3>Larry</h3>
+                        <h5 class="light grey-text text-lighten-3">El mejor amigo</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://diarioportal.com/wp-content/uploads/2019/11/perros.png">
+                    <!-- random image -->
+                    <div class="caption left-align">
+                        <h3>Coqueta</h3>
+                        <h5 class="light grey-text text-lighten-3">La mas cariñosa</h5>
+                    </div>
+                </li>
+                <li>
+                    <img
+                        src="https://iaabcjournal.org/espanol/wp-content/uploads/2018/12/street-dogs.jpg">
+                    <!-- random image -->
+                    <div class="caption right-align">
+                        <h3>Los osos</h3>
+                        <h5 class="light grey-text text-lighten-3">Lo mas inseparables</h5>
+                    </div>
+                </li>
+                <li>
+                    <img
+                        src="https://www.elsoldemexico.com.mx/republica/sociedad/yhckub-plaga-de-gatos-callejeros-invade-el-sur-de-tamaulipas/ALTERNATES/LANDSCAPE_1140/Plaga%20de%20gatos%20callejeros%20invade%20el%20sur%20de%20Tamaulipas">
+                    <!-- random image -->
+                    <div class="caption center-align">
+                        <h3>Karla</h3>
+                        <h5 class="light grey-text text-lighten-3">La mas bella</h5>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="section white">
+            <div class="row container">
+                <h2 class="header">Adopta ahora, cambia su mundo</h2>
+                <p class="grey-text text-darken-3 lighten-3">AHORA, es muy facil adoptar, simplemente da clic sobre la huella del el amigo que mas toco tu corazon
+                 y te redireccionara al formulario</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="https://i.pinimg.com/originals/4b/6e/34/4b6e3443fdda61a1176fadd515bb9cce.jpg"
+                                 height="200px">
+                        </div>
+                        <div class="card-action">
+                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i
+                                    class="material-icons">pets</i></a>
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title black-text">Camis</span>
+                            <span class="card-title grey-text">Edad: 3 meses</span>
+                            <span class="card-title grey-text">Hermosa y cariñosa</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="https://fotos.perfil.com/2019/11/19/trim/728/500/gatos-callejeros-806767.jpg"
+                                 height="200px">
+                        </div>
+                        <div class="card-action">
+                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i
+                                    class="material-icons">pets</i></a>
+                        </div>
+                        <div class="card-content">
+                             <span class="card-title black-text">Gris</span>
+                            <span class="card-title grey-text">Edad: 18 meses</span>
+                            <span class="card-title grey-text">Jugueton y mal caracter</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="https://www.tuotrodiario.com/images/025c-0f2d43853899-b429df8f59c3-1000/horizontal-800/perros-callejeros.jpg"
+                                 height="200px">
+                        </div>
+                        <div class="card-action">
+                            <a class="btn-floating halfway-fab waves-effect waves-light red">
+                                <i class="material-icons">pets</i></a>
+                        </div>
+                        <div class="card-content">
+                             <span class="card-title black-text">Hachis</span>
+                            <span class="card-title grey-text">Edad: 25 meses</span>
+                            <span class="card-title grey-text">Leal y protector</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="parallax-container">
+            <div class="parallax">
+                <img src="https://media.revistagq.com/photos/5d3199bcf96ef400080aa17d/16:9/w_1920,c_limit/queremos%20mas%20a%20perros%20que%20parejas.jpg">
+                <div class="caption center-align">
+                    <h1>hola</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="section white">
+            <div class="row container">
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="responsive-img circle" src="https://static.iris.net.co/sostenibilidad/upload/images//2019/12/12/48070_1.jpg"
+                                 height="250px">
+                        </div>
+                        <div class="card-action">
+                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i
+                                    class="material-icons">pets</i></a>
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title black-text">Oso</span>
+                            <span class="card-title grey-text">Edad: 50 meses</span>
+                            <span class="card-title grey-text">Amigable y protector</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="https://i.promecal.es/IMG/2020/E27CB384-A53B-963C-3B1911EC3FE84207.JPG"
+                                 height="250px">
+                        </div>
+                        <div class="card-action">
+                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i
+                                    class="material-icons">pets</i></a>
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title black-text">Negro</span>
+                            <span class="card-title grey-text">Edad: 23 meses</span>
+                            <span class="card-title grey-text">Leal y cariñoso</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="https://t2.ea.ltmcdn.com/es/images/6/2/7/como_ayudar_a_los_gatos_callejeros_24726_600_square.jpg"
+                                 height="250px">
+                        </div>
+                        <div class="card-action">
+                            <a class="btn-floating halfway-fab waves-effect waves-light red">
+                                <i class="material-icons">pets</i></a>
+                        </div>
+                        <div class="card-content">
+                           <span class="card-title black-text">Nieves</span>
+                            <span class="card-title grey-text">Edad: 3 meses</span>
+                            <span class="card-title grey-text">Hermosa y cariñosa</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="section white">
+            
+        </div>
+
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">ARCA</h5>
+                        <p class="grey-text text-lighten-4">Tu puedes ayudar, para que este proyecto siga en pie. Con tu
+                        donacion haremos que este proyecto sea mundial</p>
+                    </div>
+                    <div class="col l4 offset-l2 s12">
+                        <h5 class="white-text">Contacto</h5>
+                        <ul>
+                            <i class="material-icons">face</i>
+                            <i class="material-icons">location_on</i>
+                            <i class="material-icons">local_phone</i>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    © 2020 Copyright ARCA
+                    <a class="grey-text text-lighten-4 right" href="#!">Mas info</a>
+                </div>
+            </div>
+        </footer>
+
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+
+                var elems = document.querySelectorAll('.slider');
+
+                var instances = M.Slider.init(elems, {
+                    indicators: false,
+                    height: 500,
+                    interval: 3000
+
+                });
+
+                var elems = document.querySelectorAll('.parallax');
+                var instances = M.Parallax.init(elems, {
+                    responsiveThreshold: 1
+                });
+
+                var elems = document.querySelectorAll('.sidenav');
+                var instances = M.Sidenav.init(elems, {
+
+                });
+
+            });
+
+
+        </script>
+
+
+
     </body>
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </html>
