414521dc716ce83d46584306895ee254e958d42f
ivanz <ivanz@DESKTOP-SD2PMFE>
2020-06-20 13:37:14 -0500

    ARCA-ESTRUCTURADO--prueba

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <link rel="stylesheet" href="style.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <title>ARCA</title>
+        <title>ArcaPets</title>
     </head>
     <body>
         <ul id="dropdown1" class="dropdown-content">
